a920580b11bebb3622b2dbf622a5750fbb792262
hankw <hankwan777@gmail.com>
2021-11-21 23:32:06 +0900

    화면전환: 목록 -> 상세 (아직 상세 -> 목록은 안됨) appendChild 로 덧붙이던 내용을 innerHTML 로 밀어버림 DOM api도 점점 줄여나감( document.createElement 등 )

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json"; // hacker-news individual items
 
-// 중복된 기능을 하나로 묶음
 function getData(url) {
 	ajax.open("GET", url, false);
 	ajax.send();
@@ -17,28 +16,35 @@ function getData(url) {
 const newsFeed = getData(NEWS_URL);
 const ul = document.createElement("ul");
 
+// ui를 문자열로 전환
+// 상세화면으로 전환하기 위해 기존에 제목을 덧붙이던 동작 삭제
 window.addEventListener("hashchange", function () {
 	const id = location.hash.substr(1);
 	const newsContent = getData(CONTENT_URL.replace("@id", id));
-	const title = document.createElement("h1");
 
-	title.innerHTML = newsContent.title;
-	content.appendChild(title);
+	container.innerHTML = `
+		<h1>${newsContent.title}</h1>
+		<div>
+			<a href="#">목록으로</a>
+		</div>
+	`;
 });
 
+// 목록화면을 appendChild로 덧붙이는 방식에서 innerHTML 로 싹 밀어버림
+// ui형태가 ul이 바깥에서 감싸고 li>a 가 반복되는 형태
+// 배열을 사용하여 마지막에 하나로 합치기
+const newsList = [];
+newsList.push("<ul>");
 for (let i = 0; i < newsFeed.length; i++) {
-	const div = document.createElement("div");
-	div.innerHTML = `
-		<li>
-			<a href="#${newsFeed[i].id}">
-				${newsFeed[i].title}(${newsFeed[i].comments_count})
-			</a>
-		</li>
-	`;
-
-	// ul.appendChild(div.children[0]);
-	ul.appendChild(div.firstElementChild);
+	newsList.push(`
+	<li>
+		<a href="#${newsFeed[i].id}">
+		${newsFeed[i].title}(${newsFeed[i].comments_count})
+		</a>
+	</li>
+	`);
 }
+newsList.push("</ul>");
 
-container.appendChild(ul);
-container.appendChild(content);
+// 현재 뒤로가기는 기능하지 않는 상태
+container.innerHTML = newsList.join("");
