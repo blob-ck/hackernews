9e9dfdc551d078083697a0aeaeb702ba3b758340
hankw <hankwan777@gmail.com>
2021-11-21 23:48:06 +0900

    라우터 생성 : hash 값으로 분기를 나눠 화면렌더링 함수 호출 라우팅을 위해 상세화면 및 목록 렌더링 로직을 재사용 가능하도록 함수로 변경

--- app.js
+++ app.js
@@ -13,12 +13,31 @@ function getData(url) {
 	return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
+// 화면전환을 중계하는 라우터를 작성하기 전,
+// 화면전환시 목록 렌더링을 호출하므로
+// 목록을 그려낼 소스를 재사용가능하도록 함수로 만든다.
+function newsFeed() {
+	const newsFeed = getData(NEWS_URL);
+	const newsList = [];
 
-// ui를 문자열로 전환
-// 상세화면으로 전환하기 위해 기존에 제목을 덧붙이던 동작 삭제
-window.addEventListener("hashchange", function () {
+	newsList.push("<ul>");
+
+	for (let i = 0; i < newsFeed.length; i++) {
+		newsList.push(`
+	<li>
+		<a href="#${newsFeed[i].id}">
+		${newsFeed[i].title}(${newsFeed[i].comments_count})
+		</a>
+	</li>
+	`);
+	}
+
+	newsList.push("</ul>");
+
+	container.innerHTML = newsList.join("");
+}
+
+function newsDetail() {
 	const id = location.hash.substr(1);
 	const newsContent = getData(CONTENT_URL.replace("@id", id));
 
@@ -28,23 +47,21 @@ window.addEventListener("hashchange", function () {
 			<a href="#">목록으로</a>
 		</div>
 	`;
-});
-
-// 목록화면을 appendChild로 덧붙이는 방식에서 innerHTML 로 싹 밀어버림
-// ui형태가 ul이 바깥에서 감싸고 li>a 가 반복되는 형태
-// 배열을 사용하여 마지막에 하나로 합치기
-const newsList = [];
-newsList.push("<ul>");
-for (let i = 0; i < newsFeed.length; i++) {
-	newsList.push(`
-	<li>
-		<a href="#${newsFeed[i].id}">
-		${newsFeed[i].title}(${newsFeed[i].comments_count})
-		</a>
-	</li>
-	`);
 }
-newsList.push("</ul>");
 
-// 현재 뒤로가기는 기능하지 않는 상태
-container.innerHTML = newsList.join("");
+// hash값의 변화에 따라 화면전환을 하므로,
+// hashchange 이벤트로 router를 등록하고 hash값으로 화면전환한다.
+function router() {
+	const routePath = location.hash;
+
+	// location.hash 에 # 만 있을 경우에는 빈 문자열을 반환한다.
+	if (routePath === "") {
+		newsFeed();
+	} else {
+		newsDetail();
+	}
+}
+
+window.addEventListener("hashchange", router);
+
+router();
